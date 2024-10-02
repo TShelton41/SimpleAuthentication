af6831948b6f231210d6adde77fb375fd1dc6f46
Todd Shelton <TShelton41@users.noreply.github.com>
2024-10-02 14:28:22 -0400

    I have added the tabs, auth folders and screens. I have left out Clerk so you will need to add that.

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -3,24 +3,29 @@
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
-const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
+const tintColorLight = "#0a7ea4";
+const tintColorDark = "#fff";
 
 export const Colors = {
   light: {
-    text: '#11181C',
-    background: '#fff',
+    text: "#11181C",
+    background: "#fff",
     tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
+    icon: "#687076",
+    tabIconDefault: "#687076",
     tabIconSelected: tintColorLight,
   },
   dark: {
-    text: '#ECEDEE',
-    background: '#151718',
+    text: "#ECEDEE",
+    background: "#151718",
     tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
+    icon: "#9BA1A6",
+    tabIconDefault: "#9BA1A6",
     tabIconSelected: tintColorDark,
   },
+  DEV_PRIMARY: "#593D2D",
+  GRAY: "#687076",
+  DEV_DARK: "#36251B",
+  DEV_LIGHT: "##D9946C",
+  DEV_MEDIUM: "##875C44",
 };
